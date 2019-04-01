52b0ab3912cc2e1ff0e11c6ee38c7621fb4e9643
Oleksii Nikishkin <aleks.nikishin.mk@gmail.com>
2019-04-01 22:44:08 +0300

    Added email validation method

--- src/ValidateEmails/EmailValidator.php
+++ src/ValidateEmails/EmailValidator.php
@@ -8,18 +8,17 @@ class EmailValidator {
     private $_domainPattern = '[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?';
     private $_namePattern = '[A-Za-z0-9._%+-]+';
     private $_emailPattern;
-    private $_errors = [];
+    private $_emailErrors = [];
     private $_blacklistedNames = [];
     private $_blacklistedDomains = [];
     private $_emails = [];
 
-    public function __construct(array $blacklistedNames, array $blacklistedDomains, array $emails) {
+    public function __construct(array $emails, array $blacklistedNames = [], array $blacklistedDomains = []) {
+        $this->_emails = $emails;
         $this->_blacklistedNames = $blacklistedNames;
         $this->_blacklistedDomains = $blacklistedDomains;
-        $this->_emails = $emails;
 
         $this->_emailPattern = "/^" . $this->_namePattern . "@" . $this->_domainPattern . "$/";
-
     }
 
     public function validateDomains() {
@@ -30,9 +29,7 @@ class EmailValidator {
                 $invalidDomains[] = $domain;
         }
         if (count($invalidDomains))
-            throw new EmailValidateException('Invalid domain(s) given.', 400, null, [
-                'invalid_domains' => $invalidDomains
-            ]);
+            return $invalidDomains;
 
         return true;
     }
@@ -45,10 +42,42 @@ class EmailValidator {
                 $invalidNames[] = $name;
         }
         if (count($invalidNames))
-            throw new EmailValidateException('Invalid name(s) given.', 400, null, [
-                'invalid_names' => $invalidNames
-            ]);
+            return $invalidNames;
 
         return true;
+    }
+
+    public function validateEmails() {
+        $validEmails = $invalidEmails = [];
+
+        foreach ($this->_emails as $email) {
+            $emailErrors = [];
+
+            if (!preg_match($this->_emailPattern, $email)) {
+                $invalidEmails[$email] = ["email is invalid"];
+                continue;
+            }
+
+            list($name, $domain) = explode('@', $email);
+
+            if (!empty($blacklistedNames) && self::inArrayCI($name, $blacklistedNames))
+                $emailErrors[] = "the local part of the email address is blacklisted";
+
+            if (!empty($blacklistedDomains) && self::inArrayCI($domain, $blacklistedDomains))
+                $emailErrors[] = "the domain of the email address is blacklisted";
+
+            if (preg_match('/(.)\1{2}/', $email))
+                $emailErrors[] = "the email has 3 or more same characters consecutively!";
+
+            if (empty($emailErrors))
+                $validEmails[] = $email;
+            else
+                $invalidEmails[$email] = $emailErrors;
+        }
+
+        return [
+            'valid_emails' => $validEmails,
+            'invalid_emails' => $invalidEmails
+        ];
     }
 }
